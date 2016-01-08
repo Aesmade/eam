b04e772b3f003f5646d438e52807cfda1714c3e6
Konstantinos Triantafyllou <kostas.tri@hotmail.com>
2016-01-08 12:26:31 +0200

    Minor changes in css and the header.
    * The main font size is 15px now.
    * Split login in and signup options of the header and push them to the
      right

--- header.php
+++ header.php
@@ -2,29 +2,32 @@
 <html lang="en">
 
 <head>
-    <link rel="stylesheet" href="styles/main.css" />
+    <meta charset="UTF-8">
+    <title>UOA Library</title>
+
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link rel="stylesheet" href="styles/main.css" />
     <!-- JQuery -->
     <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
     <!-- Latest compiled and minified JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-    <meta charset="UTF-8">
-    <title>UOA Library</title>
 </head>
 
 <body>
     <div class="container">
         <!-- Header start -->
         <nav class="navbar navbar-default">
-            <div class="container">
-                <div class="navbar-header"><a href="#" class="navbar-brand">University of Athens</a></div>
-                <div class="collapse navbar-collapse">
+            <div class="container-fluid">
+                <div class="navbar-collapse collapse">
                     <ul class="nav navbar-nav">
+                        <li>
+                            <a href="index.php">Αρχική</a>
+                        </li>
                         <li class="dropdown">
-                            <a aria-haspopup="true" aria-expanded="false" data-toggle="dropdown" class="dropdown-toggle">Αναζήτηση<span class="caret"></span></a>
+                            <a data-toggle="dropdown" class="dropdown-toggle">Αναζήτηση</a>
                             <ul class="dropdown-menu">
-                                <li><a href="#">Απλή αναζήτηση</a></li>
+                                <li><a href="register.php">Απλή αναζήτηση</a></li>
                                 <li><a href="#">Προχωρημένη αναζήτηση</a></li>
                                 <li><a href="#">Οδηγοί αναζήτησης</a></li>
                             </ul>
@@ -67,73 +70,67 @@
                                 <li><a href="#">Προσβασιμότητα</a></li>
                             </ul>
                         </li>
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
 <?php
     if (isset($_SESSION['user'])) {
 ?>
-                            <li class="dropdown">
-                                <a aria-haspopup="true" aria-expanded="false" data-toggle="dropdown" class="dropdown-toggle">Προφίλ<span class="caret"></span></a>
-                                <ul class="dropdown-menu">
-                                    <li><a href="#">Δανεισμένα βιβλία</a></li>
-                                    <li><a href="#">Ιστορικό δανεισμών</a></li>
-                                    <li><a href="#">Ρυθμίσεις</a></li>
-                                </ul>
-                            </li>
+                        <li class="dropdown">
+                            <a aria-haspopup="true" aria-expanded="false" data-toggle="dropdown" class="dropdown-toggle">Προφίλ<span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Δανεισμένα βιβλία</a></li>
+                                <li><a href="#">Ιστορικό δανεισμών</a></li>
+                                <li><a href="#">Ρυθμίσεις</a></li>
+                            </ul>
+                        </li>
 <?php
     } else {
 ?>
-                                <li>
-                                    <a aria-haspopup="true" aria-expanded="false" data-toggle="popover" role="button" data-title="Σύνδεση" class="login-btn">Σύνδεση/Εγγραφή</a>
-                                    <div class="hide">
-                                        <form action="login.php" method="post" class="form-horizontal">
-                                            <div class="form-group">
-                                                <label for="name_inp" class="col-sm-4 control-label">Όνομα</label>
-                                                <div class="col-sm-8">
-                                                    <input type="text" name="name" id="name_inp" class="form-control" placeholder="Όνομα">
-                                                </div>
-                                            </div>
-                                            <div class="form-group">
-                                                <label for="pass_inp" class="col-sm-4 control-label">Κωδικός</label>
-                                                <div class="col-sm-8">
-                                                    <input type="password" name="password" id="pass_inp" class="form-control" placeholder="Κωδικός">
-                                                </div>
-                                            </div>
-                                            <div class="form-group">
-                                                <div class="col-sm-offset-4 col-sm-8">
-                                                    <div class="checkbox">
-                                                        <label>
-                                                            <input type="checkbox" name="" id="">Αυτόματη σύνδεση</label>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="form-group">
-                                                <div class="col-sm-offset-4 col-sm-8">
-                                                    <button type="submit" class="btn btn-default">Σύνδεση</button>
-                                                </div>
-                                            </div>
-                                            <div class="text-right small">
-                                                <a href="#">Ξέχασα τον κωδικό μου</a>
-                                            </div>
-                                            <hr />
-                                            <div class="text-right small">
-                                                <a href="#">Δημιουργία λογαριασμού</a>
-                                            </div>
-                                        </form>
+                        <li>
+                            <a aria-haspopup="true" aria-expanded="false" data-toggle="popover" data-trigger="click" role="button" id="login-btn">Σύνδεση</a>
+                            <div class="hide">
+                                <form action="login.php" method="post" class="form-horizontal">
+                                    <div class="form-group">
+                                        <label for="name_inp" class="col-sm-4 control-label">E-mail</label>
+                                        <div class="col-sm-8">
+                                            <input type="text" name="name" id="name_inp" class="form-control" placeholder="E-mail">
+                                        </div>
                                     </div>
-                                    <script>
-                                    (function($) {
-                                        $(function() {
-                                            $('.login-btn').popover({
-                                                container: 'body',
-                                                placement: 'bottom',
-                                                html: true,
-                                                content: function() {
-                                                    return $(this).next().html();
-                                                }
-                                            });
-                                        });
-                                    })(jQuery);
-                                    </script>
-                                </li>
+                                    <div class="form-group">
+                                        <label for="pass_inp" class="col-sm-4 control-label">Κωδικός</label>
+                                        <div class="col-sm-8">
+                                            <input type="password" name="password" id="pass_inp" class="form-control" placeholder="Κωδικός">
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="col-sm-offset-4 col-sm-8">
+                                            <button type="submit" class="pull-right btn btn-primary">Σύνδεση</button>
+                                        </div>
+                                    </div>
+                                    <hr/>
+                                    <div class="text-right small">
+                                        <a href="#">Ξέχασα τον κωδικό μου</a>
+                                    </div>
+                                </form>
+                            </div>
+                            <script>
+                            (function($) {
+                                $(function() {
+                                    $('#login-btn').popover({
+                                        container: 'body',
+                                        placement: 'bottom',
+                                        html: true,
+                                        content: function() {
+                                            return $(this).next().html();
+                                        }
+                                    });
+                                });
+                            })(jQuery);
+                            </script>
+                        </li>
+                        <li>
+                            <a href="register.php">Εγγραφή</a>
+                        </li>
 <?php
     }
 ?>
