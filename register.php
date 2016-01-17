450ae8f8ef1177b3d9ade9a76b628b2676c6c557
Konstantinos Triantafyllou <kostas.tri@hotmail.com>
2016-01-17 03:19:55 +0200

    Improve register form appearance.
    * Register form is now smaller and centered.
    * Error messages appear on user input mistakes and hide if the errors get
      corrected (responsive to user input).

--- register.php
+++ register.php
@@ -59,54 +59,57 @@
     include 'include/php/header.php';
 ?>
     <div class="container">
-        <div class="box">
-            <div class="box-header">Εγγραφή</div>
+        <div class="box" id="register-form-box">
             <form action="register.php" class="form-horizontal" id="register-form" method="post">
-                <div class="form-group has-feedback has-error">
-                    <label for="username" class="control-label col-sm-3">Όνομα χρήστη</label>
-                    <div class="col-sm-offset-1 col-sm-8">
-                        <input type="text" name="username" id="username" placeholder="Όνομα χρήστη" class="form-control"><span class="glyphicon glyphicon-check form-control-feedback"></span>
-                        <span class="glyphicon glyphicon-remove form-control-feedback"></span></div>
+                <div class="form-group has-feedback">
+                    <label for="name" class="control-label col-sm-4">Όνομα</label>
+                    <div class="col-sm-8">
+                        <input type="text" name="name" id="name" placeholder="Το όνομα σας" class="form-control danger-tooltip"
+                               data-toggle="tooltip" data-placement="right" title="Το όνομα σας;">
+                        <span class="glyphicon glyphicon-check form-control-feedback nodisplay"></span>
+                        <span class="glyphicon glyphicon-remove form-control-feedback nodisplay"></span></div>
                 </div>
-                <div class="form-group has-feedback has-error">
-                    <label for="name" class="control-label col-sm-3">Όνομα</label>
-                    <div class="col-sm-offset-1 col-sm-8">
-                        <input type="text" name="name" id="name" placeholder="Όνομα" class="form-control"><span class="glyphicon glyphicon-check form-control-feedback"></span>
-                        <span class="glyphicon glyphicon-remove form-control-feedback"></span></div>
+                <div class="form-group has-feedback">
+                    <label for="surname" class="control-label col-sm-4">Επώνυμο</label>
+                    <div class="col-sm-8">
+                        <input type="text" name="surname" id="surname" placeholder="Το επώνυμο σας" class="form-control danger-tooltip"
+                               data-toggle="tooltip" data-placement="right" title="Το επιθετό σας;">
+                        <span class="glyphicon glyphicon-check form-control-feedback nodisplay"></span>
+                        <span class="glyphicon glyphicon-remove form-control-feedback nodisplay"></span></div>
                 </div>
-                <div class="form-group has-feedback has-error">
-                    <label for="surname" class="control-label col-sm-3">Επώνυμο</label>
-                    <div class="col-sm-offset-1 col-sm-8">
-                        <input type="text" name="surname" id="surname" placeholder="Επώνυμο" class="form-control"><span class="glyphicon glyphicon-check form-control-feedback"></span>
-                        <span class="glyphicon glyphicon-remove form-control-feedback"></span></div>
+                <div class="form-group has-feedback">
+                    <label for="email" class="control-label col-sm-4">E-mail</label>
+                    <div class="col-sm-8">
+                        <input type="email" name="email" id="email" placeholder="Το e-mail σας" class="form-control danger-tooltip"
+                               data-toggle="tooltip" data-placement="right" title="Το e-mail δεν έχει σωστή μορφή!">
+                        <span class="glyphicon glyphicon-check form-control-feedback nodisplay"></span>
+                        <span class="glyphicon glyphicon-remove form-control-feedback nodisplay"></span></div>
                 </div>
-                <div class="form-group has-feedback has-error">
-                    <label for="email" class="control-label col-sm-3">E-mail</label>
-                    <div class="col-sm-offset-1 col-sm-8">
-                        <input type="email" name="email" id="email" placeholder="E-mail" class="form-control"><span class="glyphicon glyphicon-check form-control-feedback"></span>
-                        <span class="glyphicon glyphicon-remove form-control-feedback"></span></div>
+                <div class="form-group has-feedback">
+                    <label for="password" class="control-label col-sm-4">Κωδικός</label>
+                    <div class="col-sm-8">
+                        <input type="password" name="password" id="password" placeholder="Επιλέξτε κωδικό" class="form-control danger-tooltip"
+                               data-toggle="tooltip" data-placement="right" title="Εισάγετε τον κωδικό σας!">
+                        <span class="glyphicon glyphicon-check form-control-feedback nodisplay"></span>
+                        <span class="glyphicon glyphicon-remove form-control-feedback nodisplay"></span></div>
                 </div>
-                <div class="form-group has-feedback has-error">
-                    <label for="password" class="control-label col-sm-3">Κωδικός</label>
-                    <div class="col-sm-offset-1 col-sm-8">
-                        <input type="password" name="password" id="password" placeholder="Κωδικός" class="form-control"><span class="glyphicon glyphicon-check form-control-feedback"></span>
-                        <span class="glyphicon glyphicon-remove form-control-feedback"></span></div>
-                </div>
-                <div class="form-group has-feedback has-error">
-                    <label for="password2" class="control-label col-sm-3">Επανάληψη κωδικού</label>
-                    <div class="col-sm-offset-1 col-sm-8">
-                        <input type="password" name="password2" id="password2" placeholder="Επανάληψη κωδικού" class="form-control"><span class="glyphicon glyphicon-check form-control-feedback"></span>
-                        <span class="glyphicon glyphicon-remove form-control-feedback"></span></div>
+                <div class="form-group has-feedback">
+                    <label for="password2" class="control-label col-sm-4">Επανάληψη Κωδικού</label>
+                    <div class="col-sm-8">
+                        <input type="password" name="password2" id="password2" placeholder="Εισάγετε ξανά τον κωδικό" class="form-control danger-tooltip"
+                               data-toggle="tooltip" data-placement="right" title="Οι κωδικοί πρέπει να είναι ίδιοι!">
+                        <span class="glyphicon glyphicon-check form-control-feedback nodisplay"></span>
+                        <span class="glyphicon glyphicon-remove form-control-feedback nodisplay"></span></div>
                 </div>
 <?php
     $style = ($errors['EMPTY_VALUES_ERROR'] ? "" : "display: none");
-    print "<div id=\"fill-fields\" role=\"alert\" class=\"alert alert-danger col-sm-offset-4 col-sm-8\" style=\"$style\">Συμπληρώστε τα παραπάνω πεδία!</div>\n";
+    print "<div id=\"fill-fields\" role=\"alert\" class=\"alert alert-danger col-sm-offset-4 col-sm-8\" style=\"$style\"><strong>Σφάλμα!</strong> Συμπληρώστε τα παραπάνω πεδία!</div>\n";
     $style = ($errors['BAD_EMAIL_ERROR'] ? "" : "display: none");
-    print "<div id=\"invalid-email\" role=\"alert\" class=\"alert alert-danger col-sm-offset-4 col-sm-8\" style=\"$style\">Το e-mail δεν έχει σωστή μορφή!</div>\n";
+    print "<div id=\"invalid-email\" role=\"alert\" class=\"alert alert-danger col-sm-offset-4 col-sm-8\" style=\"$style\"><strong>Σφάλμα!</strong> Το e-mail δεν έχει σωστή μορφή!</div>\n";
     $style = ($errors['EMAIL_EXISTS_ERROR'] ? "" : "display: none");
-    print "<div role=\"alert\" class=\"alert alert-danger col-sm-offset-4 col-sm-8\" style=\"$style\">Το email χρησιμοποιείται ήδη!</div>\n";
+    print "<div role=\"alert\" class=\"alert alert-danger col-sm-offset-4 col-sm-8\" style=\"$style\"><strong>Σφάλμα!</strong> Το email χρησιμοποιείται ήδη!</div>\n";
     $style = ($errors['PASSWORDS_DIFFER_ERROR'] ? "" : "display: none");
-    print "<div id=\"passwords-no-match\" role=\"alert\" class=\"alert alert-danger col-sm-offset-4 col-sm-8\" style=\"$style\">Οι κωδικοί δεν είναι ίδιοι!</div>\n";
+    print "<div id=\"passwords-no-match\" role=\"alert\" class=\"alert alert-danger col-sm-offset-4 col-sm-8\" style=\"$style\"><strong>Σφάλμα!</strong> Οι κωδικοί δεν είναι ίδιοι!</div>\n";
 ?>
                 <div class="text-right">
                     <input type="submit" name="register" id="submit-button" class="btn btn-primary btn-lg" value="Εγγραφή">
@@ -114,6 +117,11 @@
             </form>
         </div>
     </div>
+    <script>
+    $(document).ready(function(){
+        $('[data-toggle="tooltip"]').tooltip({trigger: 'manual'}).tooltip("hide");
+    });
+    </script>
     <script>
     (function($) {
         $(function() {
@@ -124,9 +132,8 @@
                 },
                 errorMsg: '#fill-fields'
             };
-            // each field has a check function and a message if the check fails
+            // each field has a check function
             var checks = {
-                '#username': hasLengthCheck,
                 '#name': hasLengthCheck,
                 '#surname': hasLengthCheck,
                 '#email': {
@@ -138,46 +145,60 @@
                 '#password': hasLengthCheck,
                 '#password2': {
                     isValid: function(str) {
-                        return str == $('#password').val();
+                        return str.length > 0 && str == $('#password').val();
                     },
                     errorMsg: '#passwords-no-match'
                 }
             };
-            for (chk in checks) {
-                var elem = $(chk);
-                var par = elem.parent().parent();
-                // provide the function called whenever a field value changes
-                $(chk).on('input', (function(element, parent, checkObj) {
-                    // closure for the elem, par and checks[c] vars
-                    return function() {
-                        // if the check succeeds, mark the field and hide the error message
-                        if (checkObj.isValid(element.val())) {
-                            parent.removeClass('has-error');
-                            parent.addClass('has-success');
-                        } else {
-                            parent.removeClass('has-success');
-                            parent.addClass('has-error');
-                        }
-                    };
-                })(elem, par, checks[chk]));
-            }
-            // also check password2 when password changes
-            var pass2Elem = $('#password2');
-            var pass2Par = pass2Elem.parent().parent();
-            $('#password').on('input', function() {
-                if (checks['#password2'].isValid(pass2Elem.val())) {
-                    pass2Par.removeClass('has-error');
-                    pass2Par.addClass('has-success');
+
+            // function called whenever a field value changes
+            function checker(check, displayed) {
+                var element = $(check);
+                var parent = element.parent().parent();
+                var checkObj = checks[check];
+
+                if (checkObj.isValid(element.val())) {
+                    if (displayed == true || displayed == null) {
+                        element.tooltip("hide");
+                        displayed = false;
+                    }
+                    parent.removeClass('has-error');
+                    parent.addClass('has-success');                    
                 } else {
-                    pass2Par.removeClass('has-success');
-                    pass2Par.addClass('has-error');
+                    if (displayed == false || displayed == null) {
+                        element.tooltip("show");
+                        displayed = true;
+                    }
+                    parent.removeClass('has-success');
+                    parent.addClass('has-error');                    
                 }
-            });
+                return displayed;
+            };
+            
+            var delay = (function(){
+                var timer = 0;
+                return function(callback, ms){
+                    clearTimeout (timer);
+                    timer = setTimeout(callback, ms);
+                };
+            })();
+
+            for (chk in checks) {
+                $(chk).on('input', (function(check) {
+                    var displayed = null;
+                    return function() {
+                        delay(function() {
+                            displayed = checker(check, displayed);
+                            if (check == '#password') $('#password2').trigger('input');
+                        }, 200);
+                    }
+                })(chk));
+            }
+
             $('#register-form').submit(function(event) {
                 $('.alert.alert-danger').hide();
                 for (chk in checks) {
-                    if (!checks[chk].isValid($(chk).val())) {
-                        $(checks[chk].errorMsg).fadeIn();
+                    if (checker(chk, null)) {
                         event.preventDefault();
                     }
                 }
@@ -185,6 +206,5 @@
         });
     })(jQuery);
     </script>
-<?php
-    include 'include/php/footer.php';
-?>
+</body>
+</html>
