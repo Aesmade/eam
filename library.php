a082e98e9bd0a4542008c7c95b7231981a6f5a9f
Konstantinos Triantafyllou <kostas.tri@hotmail.com>
2016-01-19 13:43:35 +0200

    Redesign library page and make it dynamic.
    * Loads information about a library (specified by the GET request)
      by querying the database.
    * The loaded image is not loaded from the database yet.
      All the library pages show the same image.

---
+++ library.php
@@ -0,0 +1,138 @@
+<?php
+    include 'include/php/db_connect.php';
+    include 'include/php/helpers.php';
+
+    if (isset($_GET['id'])) {
+        $id = $_GET['id'];
+
+        $query = "SELECT name, description, address, latitude, longitude, " .
+            "telephone, email, opening_time, closing_time FROM `Library` where id = ?";
+        $stmt = $db->prepare($query);
+        $stmt->bind_param('i', $id);
+        $stmt->execute();
+        $stmt->store_result();
+        if ($stmt->num_rows == 1) {
+            $stmt->bind_result($name, $descr, $address, $lat, $lon, $tel, $email, $open_time, $close_time);
+            $stmt->fetch();
+
+        }
+        $stmt->free_result();
+        $stmt->close();
+        $db->close();
+    }
+
+    // include header.php and library.css
+    $styles = array("%OTHER_STYLESHEET_1%" => "rel=\"stylesheet\" href=\"styles/library.css\"");
+    echo replace_contents('include/php/header.php', $styles);
+?>
+    <div class="container">
+        <ol class="breadcrumb">
+            <li><a href="index.php">Αρχική</a></li>
+            <li><a href="libraries_calendar.php">Βιβλιοθήκες</a></li>
+            <li><span class="active"><?php echo $name ?></span></li>
+        </ol>
+        <div class="row">
+            <div class="col-sm-3">
+                <div class="box">
+                    <div class="box-header">Ώρες λειτουργίας</div>
+                    <div>
+                        <div>Σήμερα: <strong><?php echo substr($open_time, 0, 5) . " " . substr($close_time, 0, 5) ?></strong></div>
+                        <hr />
+                        <div><a href="libraries_calendar.php">Εβδομαδιαίο Ωράριο</a></div>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="box-header">Επικοινωνία</div>
+                    <div>
+                        <strong>
+                        <div><span class="glyphicon glyphicon-earphone"></span> <?php echo $tel ?></div>
+                        <div><span class="glyphicon glyphicon-envelope"></span> <?php echo $email ?></div>
+                        <hr />
+                        <div><span class="glyphicon glyphicon-map-marker"></span> <?php echo $address ?></div>
+                        </strong>
+                        <div><a href="#" data-toggle="modal" data-target="#myModal">Προβολή στο χάρτη</a></div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-9">
+                <div class="box">
+                    <div class="box-header"><?php echo $name ?></div>
+                    <div id="lib-content-left">
+                        <div><strong>Γνωστικό Αντικείμενο</strong></div>
+                        <p class="content"><?php echo $descr ?></p>
+                        <div><strong>Συλλογή Ανοικτής Πρόσβασης</strong></div>
+                        <p class="content">Η Συλλογή Ανοικτής Πρόσβασης περιλαμβάνει βιβλία, περιοδικά, οπτικοακουστικό υλικό, γκρίζα βιβλιογραφία (διδακτορικές διατριβές, διπλωματικές και πτυχιακές εργασίες), ανάτυπα εργασιών, σημειώσεις μαθημάτων, λεξικά, εγκυκλοπαίδειες και χάρτες. Τα βιβλία είναι ταξιθετημένα σύμφωνα με το δεκαδικό σύστημα ταξινόμησης Dewey, ενώ τα περιοδικά έχουν ταξιθετηθεί με απόλυτη αλφαβητική σειρά τίτλου.</p>
+                        <div><strong>Συλλογή Περιορισμένης Πρόσβασης</strong></div>
+                        <p class="content">Η Συλλογή Περιορισμένης Πρόσβασης περιλαμβάνει σπάνιο και πολύτιμο υλικό το οποίο φυλάσσεται σε ειδικά διαμορφωμένη αίθουσα. Οι χρήστες έχουν πρόσβαση ύστερα από ειδική άδεια, για περιορισμένο χρονικό διάστημα και μόνο στο χώρο της βιβλιοθήκης που θα υποδείξει το προσωπικό της</p>
+                    </div><!--
+                 --><div id="lib-content-right">
+                        <div id="lib-image-div">
+                            <img src="resources/thet.jpg" alt="" id="lib-image">
+                        </div>
+                        <div id="links-div">
+                            <div><strong>Υπηρεσίες</strong></div>
+                            <div class="content">
+                                <ul>
+                                    <li><a href="#">Αναζήτηση τίτλων</a></li>
+                                    <li><a href="#">Βιβλία & συλλογές</a></li>
+                                    <li><a href="#">Κανονισμοί δανεισμού</a></li>
+                                    <li><a href="#">Για ΑΜΕΑ</a></li>
+                                    <li><a href="#">Περισσότερες Υπηρεσίες</a></li>
+                                </ul>
+                            </div>
+                            <br />
+                            <div><strong>Ομάδες Μελέτης</strong></div>
+                            <div class="content">
+                                <ul>
+                                    <li><a href="#">Ομάδα στο Facebook</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Modal -->
+    <div id="myModal" class="modal" role="dialog">
+        <div class="modal-dialog modal-lg">
+        <!-- Modal content-->
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title"><?php echo $name ?></h4>
+                </div>
+                <div class="modal-body">
+                    <div class="embed-responsive embed-responsive-16by9">
+                        <div id="map" class="embed-responsive-item"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script type="text/javascript">
+        var myLatLng = {lat: <?php echo $lat ?>, lng: <?php echo $lon ?>};
+        var map = null;
+        function initMap() {
+            map = new google.maps.Map(document.getElementById('map'), {
+                center: myLatLng
+            });
+
+            var marker = new google.maps.Marker({
+                position: myLatLng,
+                map: map,
+                title: '<?php echo $address ?>'
+            });
+        }
+
+        $('#myModal').on('shown.bs.modal', function () {
+            google.maps.event.trigger(map, "resize");
+            map.setCenter(myLatLng);
+            map.setZoom(14);
+        });
+    </script>
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCyxoW-t_Qc4yM8gIzG_EnR_In4RSBlEuY&callback=initMap"
+    type="text/javascript"></script>
+<?php
+    include 'include/php/footer.php';
+?>
