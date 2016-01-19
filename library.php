4b2dfe581a94a1eb5ff5c6c1886a3bb37141dfc5
Konstantinos Triantafyllou <kostas.tri@hotmail.com>
2016-01-19 15:47:17 +0200

    Refactoring the code that checks the output vars.

--- library.php
+++ library.php
@@ -2,6 +2,8 @@
     include 'include/php/db_connect.php';
     include 'include/php/helpers.php';
 
+    $lib = array();
+
     if (isset($_GET['id'])) {
         $id = $_GET['id'];
 
@@ -12,9 +14,12 @@
         $stmt->execute();
         $stmt->store_result();
         if ($stmt->num_rows == 1) {
-            $stmt->bind_result($name, $descr, $address, $lat, $lon, $tel, $email, $open_time, $close_time);
+            $stmt->bind_result($lib['name'], $lib['description'], $lib['address'],
+                $lib['lat'], $lib['lon'], $lib['tel'], $lib['email'], $lib['open'], $lib['close']);
             $stmt->fetch();
-
+            foreach ($lib as $key => $value) {
+                $lib[$key] = htmlspecialchars($value);
+            }
         }
         $stmt->free_result();
         $stmt->close();
@@ -29,14 +34,14 @@
         <ol class="breadcrumb">
             <li><a href="index.php">Αρχική</a></li>
             <li><a href="libraries_calendar.php">Βιβλιοθήκες</a></li>
-            <li><span class="active"><?php echo $name ?></span></li>
+            <li><span class="active"><?php echo $lib['name'] ?></span></li>
         </ol>
         <div class="row">
             <div class="col-sm-3">
                 <div class="box">
                     <div class="box-header">Ώρες λειτουργίας</div>
                     <div>
-                        <div>Σήμερα: <strong><?php echo substr($open_time, 0, 5) . " " . substr($close_time, 0, 5) ?></strong></div>
+                        <div>Σήμερα: <strong><?php echo substr($lib['open'], 0, 5) . " - " . substr($lib['close'], 0, 5) ?></strong></div>
                         <hr />
                         <div><a href="libraries_calendar.php">Εβδομαδιαίο Ωράριο</a></div>
                     </div>
@@ -45,10 +50,10 @@
                     <div class="box-header">Επικοινωνία</div>
                     <div>
                         <strong>
-                        <div><span class="glyphicon glyphicon-earphone"></span> <?php echo $tel ?></div>
-                        <div><span class="glyphicon glyphicon-envelope"></span> <?php echo $email ?></div>
+                        <div><span class="glyphicon glyphicon-earphone"></span> <?php echo $lib['tel'] ?></div>
+                        <div><span class="glyphicon glyphicon-envelope"></span> <?php echo $lib['email'] ?></div>
                         <hr />
-                        <div><span class="glyphicon glyphicon-map-marker"></span> <?php echo $address ?></div>
+                        <div><span class="glyphicon glyphicon-map-marker"></span> <?php echo $lib['address'] ?></div>
                         </strong>
                         <div><a href="#" data-toggle="modal" data-target="#myModal">Προβολή στο χάρτη</a></div>
                     </div>
@@ -56,10 +61,10 @@
             </div>
             <div class="col-sm-9">
                 <div class="box">
-                    <div class="box-header"><?php echo $name ?></div>
+                    <div class="box-header"><?php echo $lib['name'] ?></div>
                     <div id="lib-content-left">
                         <div><strong>Γνωστικό Αντικείμενο</strong></div>
-                        <p class="content"><?php echo $descr ?></p>
+                        <p class="content"><?php echo $lib['description'] ?></p>
                         <div><strong>Συλλογή Ανοικτής Πρόσβασης</strong></div>
                         <p class="content">Η Συλλογή Ανοικτής Πρόσβασης περιλαμβάνει βιβλία, περιοδικά, οπτικοακουστικό υλικό, γκρίζα βιβλιογραφία (διδακτορικές διατριβές, διπλωματικές και πτυχιακές εργασίες), ανάτυπα εργασιών, σημειώσεις μαθημάτων, λεξικά, εγκυκλοπαίδειες και χάρτες. Τα βιβλία είναι ταξιθετημένα σύμφωνα με το δεκαδικό σύστημα ταξινόμησης Dewey, ενώ τα περιοδικά έχουν ταξιθετηθεί με απόλυτη αλφαβητική σειρά τίτλου.</p>
                         <div><strong>Συλλογή Περιορισμένης Πρόσβασης</strong></div>
@@ -100,7 +105,7 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal">&times;</button>
-                    <h4 class="modal-title"><?php echo $name ?></h4>
+                    <h4 class="modal-title"><?php echo $lib['name'] ?></h4>
                 </div>
                 <div class="modal-body">
                     <div class="embed-responsive embed-responsive-16by9">
@@ -111,7 +116,7 @@
         </div>
     </div>
     <script type="text/javascript">
-        var myLatLng = {lat: <?php echo $lat ?>, lng: <?php echo $lon ?>};
+        var myLatLng = {lat: <?php echo $lib['lat'] ?>, lng: <?php echo $lib['lon'] ?>};
         var map = null;
         function initMap() {
             map = new google.maps.Map(document.getElementById('map'), {
@@ -121,7 +126,7 @@
             var marker = new google.maps.Marker({
                 position: myLatLng,
                 map: map,
-                title: '<?php echo $address ?>'
+                title: '<?php echo $lib['address'] ?>'
             });
         }
 
