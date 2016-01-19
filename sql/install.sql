e91f8417bd598a9a9397cca04c9e13c25a52b316
Konstantinos Triantafyllou <kostas.tri@hotmail.com>
2016-01-19 21:58:13 +0200

    Update sql install script.

--- sql/install.sql
+++ sql/install.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jan 17, 2016 at 08:41 PM
+-- Generation Time: Jan 19, 2016 at 09:56 PM
 -- Server version: 5.6.27-0ubuntu0.15.04.1
 -- PHP Version: 5.6.4-4ubuntu6.4
 
@@ -19,6 +19,25 @@ SET time_zone = "+00:00";
 --
 -- Database: `eam`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Author`
+--
+
+CREATE TABLE IF NOT EXISTS `Author` (
+  `id` int(11) NOT NULL,
+  `name` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `Author`
+--
+
+INSERT INTO `Author` (`id`, `name`) VALUES
+(1, 'Brian Kernighan'),
+(2, 'Dennis Ritchie');
 
 -- --------------------------------------------------------
 
@@ -39,7 +58,7 @@ CREATE TABLE IF NOT EXISTS `Book` (
 --
 
 INSERT INTO `Book` (`isbn`, `title`, `description`, `publication_date`, `type`) VALUES
-('123-77712832', 'The C Programming Language', 'The authors present the complete guide to ANSI standard C language programming. Written by the developers of C, this new version helps readers keep up with the finalized ANSI standard for C while showing how to take advantage of C''s rich set of operators, economy of expression, improved control flow, and data structures. The 2/E has been completely rewritten with additional examples and problem sets to clarify the implementation of difficult language constructs. For years, C programmers have let K&R guide them to building well-structured and efficient programs. Now this same help is available to those working with ANSI compilers. Includes detailed coverage of the C language plus the official C language reference manual for at-a-glance help with syntax notation, declarations, ANSI changes, scope rules, and the list goes on and on. ', '1988-04-01', 'book'),
+('123-77712832', 'The C Programming Language', 'The authors present the complete guide to ANSI standard C language programming. Written by the developers of C, this new version helps readers keep up with the finalized ANSI standard for C while showing how to take advantage of C''s rich set of operators, economy of expression, improved control flow, and data structures. The 2/E has been completely rewritten with additional examples and problem sets to clarify the implementation of difficult language constructs. For years, C programmers have let K&R guide them to building well-structured and efficient programs. Now this same help is available to those working with ANSI compilers.', '1988-04-01', 'book'),
 ('9782974329-23', 'Algebra - The Very Basics', 'If you''re looking for a gentle introduction to basic mathematics, look no further. This book picks you up at the very beginning and guides you through the foundations of algebra using lots of examples and no-nonsense explanations. Each chapter contains well-chosen exercises as well as all the solutions. No prior knowledge is required. ', '2014-01-06', 'book');
 
 -- --------------------------------------------------------
@@ -84,6 +103,25 @@ INSERT INTO `Books_by_Category` (`book_isbn`, `category_id`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `Book_Authors`
+--
+
+CREATE TABLE IF NOT EXISTS `Book_Authors` (
+  `book_isbn` varchar(32) NOT NULL,
+  `author_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `Book_Authors`
+--
+
+INSERT INTO `Book_Authors` (`book_isbn`, `author_id`) VALUES
+('123-77712832', 1),
+('123-77712832', 2);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `Book_Loans`
 --
 
@@ -91,9 +129,17 @@ CREATE TABLE IF NOT EXISTS `Book_Loans` (
 `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `book_isbn` varchar(32) CHARACTER SET utf8 NOT NULL,
+  `library_id` int(11) NOT NULL,
   `start_date` date NOT NULL,
   `end_date` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `Book_Loans`
+--
+
+INSERT INTO `Book_Loans` (`id`, `user_id`, `book_isbn`, `library_id`, `start_date`, `end_date`) VALUES
+(1, 1, '123-77712832', 1, '2016-01-12', '2016-05-12');
 
 -- --------------------------------------------------------
 
@@ -128,7 +174,7 @@ CREATE TABLE IF NOT EXISTS `Library` (
   `address` varchar(128) CHARACTER SET utf8 NOT NULL,
   `latitude` double NOT NULL,
   `longitude` double NOT NULL,
-  `telephone` varchar(10) CHARACTER SET utf8 NOT NULL,
+  `telephone` varchar(12) CHARACTER SET utf8 NOT NULL,
   `email` varchar(256) CHARACTER SET utf8 NOT NULL,
   `opening_time` time NOT NULL,
   `closing_time` time NOT NULL
@@ -139,7 +185,7 @@ CREATE TABLE IF NOT EXISTS `Library` (
 --
 
 INSERT INTO `Library` (`id`, `name`, `description`, `address`, `latitude`, `longitude`, `telephone`, `email`, `opening_time`, `closing_time`) VALUES
-(1, 'Βιβλιοθήκη Σχολής Θετικών Επιστημών', 'Γνωστικό Αντικείμενο:\r\nΗ Συλλογή καλύπτει στο μεγαλύτερο μέρος της τις εξής θεματικές κατηγορίες: Βιολογία, Γεωλογία και Γεωπεριβάλλον, Μαθηματικά, Πληροφορική και Τηλεπικοινωνίες, Φαρμακευτική, Φυσική και Χημεία.\r\n\r\nΒιβλία:\r\nΣΥΛΛΟΓΗ ΑΝΟΙΚΤΗΣ ΠΡΟΣΒΑΣΗΣ\r\nΗ Συλλογή Ανοικτής Πρόσβασης περιλαμβάνει βιβλία, περιοδικά, οπτικοακουστικό υλικό, γκρίζα βιβλιογραφία (διδακτορικές διατριβές, διπλωματικές και πτυχιακές εργασίες), ανάτυπα εργασιών, σημειώσεις μαθημάτων, λεξικά, εγκυκλοπαίδειες και χάρτες. Τα βιβλία είναι ταξιθετημένα σύμφωνα με το δεκαδικό σύστημα ταξινόμησης Dewey, ενώ τα περιοδικά έχουν ταξιθετηθεί με απόλυτη αλφαβητική σειρά τίτλου. Το υλικό της συλλογής μπορεί να αναζητηθεί μέσα από τον Ανοικτό Κατάλογο Δημόσιας Πρόσβασης (OPAC: Open Public Access Catalog).\r\n\r\nΣΥΛΛΟΓΗ ΠΕΡΙΟΡΙΣΜΕΝΗΣ ΠΡΟΣΒΑΣΗΣ\r\nΗ  Συλλογή Περιορισμένης Πρόσβασης περιλαμβάνει σπάνιο και πολύτιμο υλικό το οποίο φυλάσσεται σε ειδικά διαμορφωμένη αίθουσα. Οι χρήστες έχουν πρόσβαση ύστερα από ειδική άδεια, για περιορισμένο χρονικό διάστημα και μόνο στο χώρο της βιβλιοθήκης που θα υποδείξει το προσωπικό της. Το υλικό μπορεί να αναζητηθεί μέσα από τον Ανοικτό Κατάλογο Δημόσιας Πρόσβασης (OPAC). ', 'Πανεπιστημιούπολη, Ιλίσια, 157 84 Αθήνα', 37.967259, 23.781945, ' 210726545', 'thet@lib.uoa.gr', '09:00:00', '21:00:00');
+(1, 'Βιβλιοθήκη Σχολής Θετικών Επιστημών', 'Η Συλλογή καλύπτει στο μεγαλύτερο μέρος της τις εξής θεματικές κατηγορίες: Βιολογία, Γεωλογία και Γεωπεριβάλλον, Μαθηματικά, Πληροφορική και Τηλεπικοινωνίες, Φαρμακευτική, Φυσική και Χημεία.\r\n\r\n', 'Πανεπιστημιούπολη, Ιλίσια, 157 84 Αθήνα', 37.967259, 23.781945, ' 210-726545', 'thet@lib.uoa.gr', '09:00:00', '21:00:00');
 
 -- --------------------------------------------------------
 
@@ -153,18 +199,25 @@ CREATE TABLE IF NOT EXISTS `User` (
   `last_name` varchar(30) CHARACTER SET utf8 NOT NULL,
   `email` varchar(256) CHARACTER SET utf8 NOT NULL,
   `password` varchar(32) CHARACTER SET utf8 NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=51 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `User`
 --
 
 INSERT INTO `User` (`id`, `first_name`, `last_name`, `email`, `password`) VALUES
-(1, 'test', 'test', 'test@test.gr', '6512bd43d9caa6e02c990b0a82652dca');
+(49, 'test', 'test', 'test@test.gr', '1a1dc91c907325c69271ddf0c944bc72'),
+(50, 'Γιαννης', 'Γιαννης', 'giannis@giannis.gr', '1a1dc91c907325c69271ddf0c944bc72');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `Author`
+--
+ALTER TABLE `Author`
+ ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `Book`
@@ -216,7 +269,7 @@ ALTER TABLE `User`
 -- AUTO_INCREMENT for table `Book_Loans`
 --
 ALTER TABLE `Book_Loans`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `Category`
 --
@@ -231,7 +284,7 @@ MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 -- AUTO_INCREMENT for table `User`
 --
 ALTER TABLE `User`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=51;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
