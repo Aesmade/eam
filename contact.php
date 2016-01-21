50427eb6ab8dc5505324edbfb80b3f881b5b6801
Konstantinos Triantafyllou <kostas.tri@hotmail.com>
2016-01-21 03:12:16 +0200

    Minor fixes in contact page.

--- contact.php
+++ contact.php
@@ -1,39 +1,50 @@
 <?php
-    include 'include/php/header.php';
+    include 'include/php/helpers.php';
+
+    // include header.php and contact.css
+    $styles = array('%OTHER_STYLESHEET_1%' => 'rel="stylesheet" href="styles/contact.css"');
+    echo replace_contents('include/php/header.php', $styles);
 ?>
     <div class="container">
         <ol class="breadcrumb">
-            <li><a href="#">Κεντρική</a></li>
+            <li><a href="index.php">Αρχική</a></li>
             <li><span class="active">Επικοινωνία</span></li>
         </ol>
         <div class="row">
             <div class="col-sm-7">
-                <div class="box" style="height: 630px">
+                <div id="left-box" class="box">
                     <div class="box-header">Κάντε μας μια ερώτηση</div>
                     <div class="form-group"><label for="titleInput">Τίτλος</label><input type="text" name="titleInput" id="titleInput" class="form-control" placeholder="Τίτλος"></div>
                     <div class="form-group"><label for="infoInput">Περισσότερες πληροφορίες</label><textarea name="infoInput" id="infoInput" rows="6" class="form-control" placeholder="Περισσότερες πληροφορίες"></textarea></div>
                     <div class="form-horizontal" style="margin-top: 30px">
                         <label style="padding-top: 10px; padding-bottom: 10px;">Τα στοιχεία σας</label>
-                        <div class="form-group"><label class="col-sm-3" for="nameInput">Όνομα</label><div class="col-sm-9"><input type="text" name="inputText" id="inputText" placeholder="Όνομα" class="form-control"></div></div>
-                        <div class="form-group"><label class="col-sm-3" for="nameEmail">E-mail</label><div class="col-sm-9"><input type="text" name="emailText" id="emailText" placeholder="E-mail" class="form-control"></div></div>
+                        <div class="form-group vertical-align">
+                            <label class="col-sm-3" for="nameInput">Όνομα</label>
+                            <div class="col-sm-9"><input type="text" name="inputText" id="inputText" placeholder="Όνομα" class="form-control"></div>
+                        </div>
+                        <div class="form-group vertical-align">
+                            <label class="col-sm-3" for="nameEmail">E-mail</label>
+                            <div class="col-sm-9"><input type="text" name="emailText" id="emailText" placeholder="E-mail" class="form-control"></div>
+                        </div>
                     </div>
-                    <button type="submit" style="margin-top: 20px" class="pull-right btn btn-primary">Αποστολή ερώτησης</button>
+                    <button type="submit" style="margin-top: 20px" class="pull-right btn-lg btn-primary">Αποστολή ερώτησης</button>
                 </div>
             </div>
             <div class="col-sm-5">
-                <div class="box" style="height: 300px">
+                <div id="right-box-top" class="box" style="height: 300px">
                     <div class="box-header">Συχνές ερωτήσεις</div>
-                    <div><a href="#">Πώς μπορώ να βρω ένα βιβλίο;</a></div>
-                    <div><a href="#">Πού βρίσκονται οι βιβλιοθήκες;</a></div>
-                    <div><a href="#">Πώς δανείζομαι ένα βιβλίο;</a></div>
-                    <div><a href="#">Πως μπορώ να παραγγείλω ένα άρθρο;</a></div>
-                    <div><a href="#">Πως μπορώ να εργαστώ στη βιβλιοθήκη;</a></div>
-                    <div class="text-right" style="font-size: 15px; position: absoute; bottom: 0;"><a href="#">Περισσότερες ερωτήσεις και Βοήθεια</a></div>
+                    <ul>
+                    <li><a href="#">Πώς μπορώ να αναζητήσω βιβλία;</a></li>
+                    <li><a href="#">Σε ποιες βιβλιοθήκες έχω πρόσβαση;</a></li>
+                    <li><a href="#">Πώς μπορώ να δανειστώ βιβλία;</a></li>
+                    <li><a href="#">Πώς μπορώ να παραγγείλω ένα άρθρο;</a></li>
+                    <li><a href="#">Μπορώ να εργαστώ στη βιβλιοθήκη;</a></li>
+                    <div id="more-questions-link"><a href="#">Περισσότερες ερωτήσεις - Βοήθεια</a></div>
                 </div>
-                <div class="box" style="height: 300px; position: relative;">
+                <div id="right-box-bottom" class="box">
                     <div class="box-header">Ζωντανή συζήτηση</div>
                     <div>Η συζήτηση είναι απενεργοποιημένη</div>
-                    <div class="chat-bar">
+                    <div id="chat-bar">
                         <div class="input-group">
                             <input type="text" class="form-control" disabled placeholder="Η συζήτηση είναι απενεργοποιημένη" />
                             <span class="input-group-btn">
