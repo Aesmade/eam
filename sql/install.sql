da76fea6bcf4212923f3cea83695e336c1a5dddb
Konstantinos Triantafyllou <kostas.tri@hotmail.com>
2016-01-24 21:32:11 +0200

    Update mysql install script by adding new books.

--- sql/install.sql
+++ sql/install.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jan 24, 2016 at 07:25 PM
+-- Generation Time: Jan 24, 2016 at 09:26 PM
 -- Server version: 5.6.27-0ubuntu0.15.04.1
 -- PHP Version: 5.6.4-4ubuntu6.4
 
@@ -29,7 +29,7 @@ SET time_zone = "+00:00";
 CREATE TABLE IF NOT EXISTS `Author` (
 `id` int(11) NOT NULL,
   `name` varchar(64) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `Author`
@@ -39,7 +39,10 @@ INSERT INTO `Author` (`id`, `name`) VALUES
 (1, 'Brian Kernighan'),
 (2, 'Dennis Ritchie'),
 (3, 'Tozer'),
-(4, 'Metin Bektas');
+(4, 'Metin Bektas'),
+(5, 'Gerald Schroeder'),
+(6, 'Randall Munroe'),
+(7, 'Yanis Varoufakis');
 
 -- --------------------------------------------------------
 
@@ -53,7 +56,7 @@ CREATE TABLE IF NOT EXISTS `Book` (
   `language` varchar(32) CHARACTER SET utf8 NOT NULL,
   `description` text CHARACTER SET utf8 NOT NULL,
   `publication_date` date NOT NULL,
-  `type` enum('book','magazine','paper') NOT NULL,
+  `type` enum('book','magazine','article') NOT NULL,
   `imgs` text NOT NULL,
   `imgm` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -63,10 +66,16 @@ CREATE TABLE IF NOT EXISTS `Book` (
 --
 
 INSERT INTO `Book` (`isbn`, `title`, `language`, `description`, `publication_date`, `type`, `imgs`, `imgm`) VALUES
+('0061710164', 'God According to God', 'Αγγλικά', 'In his first book since 2002’s acclaimed The Hidden Face of God, popular scientist Gerald Schroeder combines decades of scientific research and biblical study to present a groundbreaking new paradigm of how to understand God. Readers of Jack Miles’ God: A Biography, Francis Collins’s The Language of God, and Richard Friedman’s Who Wrote the Bible will find much to contemplate in God According to God.', '2010-12-24', 'book', 'http://ecx.images-amazon.com/images/I/510nI%2BlqGBL._SX330_BO1,204,203,200_.jpg', 'http://ecx.images-amazon.com/images/I/510nI%2BlqGBL._SX330_BO1,204,203,200_.jpg'),
 ('013937681X', 'The Unix Programming Environment', 'Αγγλικά', 'Designed for first-time and experienced users, this book describes the UNIX® programming environment and philosophy in detail. Readers will gain an understanding not only of how to use the system, its components, and the programs, but also how these fit into the total environment. ', '1983-07-24', 'book', 'http://ecx.images-amazon.com/images/I/41HMOpkLA2L._SX383_BO1,204,203,200_.jpg', 'http://ecx.images-amazon.com/images/I/41HMOpkLA2L._SX383_BO1,204,203,200_.jpg'),
+('0544668251', 'Thing Explainer: Complicated Stuff in Simple Words', 'Αγγλικά', 'Have you ever tried to learn more about some incredible thing, only to be frustrated by incomprehensible jargon?  Randall Munroe is here to help.  In Thing Explainer, he uses line drawings and only the thousand (or, rather, “ten hundred”) most common words to provide simple explanations for some of the most interesting stuff there is/', '2015-09-14', 'book', 'http://ecx.images-amazon.com/images/I/51Uv99xOnIL._SX344_BO1,204,203,200_.jpg', 'http://ecx.images-amazon.com/images/I/51Uv99xOnIL._SX344_BO1,204,203,200_.jpg'),
 ('123-77712832', 'The C Programming Language', 'Αγγλικά', 'The authors present the complete guide to ANSI standard C language programming. Written by the developers of C, this new version helps readers keep up with the finalized ANSI standard for C while showing how to take advantage of C''s rich set of operators, economy of expression, improved control flow, and data structures. The 2/E has been completely rewritten with additional examples and problem sets to clarify the implementation of difficult language constructs. For years, C programmers have let K&R guide them to building well-structured and efficient programs. Now this same help is available to those working with ANSI compilers.', '1988-04-01', 'book', 'http://ecx.images-amazon.com/images/I/41qX6YdIJ7L._SX379_BO1,204,203,200_.jpg', 'http://ecx.images-amazon.com/images/I/41qX6YdIJ7L._SX379_BO1,204,203,200_.jpg'),
 ('1503379744A', 'The Pursuit of God', 'Αγγλικά', '“As the heart panteth after the water brooks, so panteth my soul after thee, O God.” This thirst for an intimate relationship with God, claims A.W. Tozer, is not for a select few, but should be the experience of every follower of Christ. But, he asserts, it is all too rare when believers have become conditioned by tradition to accept standards of mediocrity, and the church struggles with formality and worldliness.', '2014-10-22', 'book', 'http://ecx.images-amazon.com/images/I/41NbdDuf0EL._SX331_BO1,204,203,200_.jpg', 'http://ecx.images-amazon.com/images/I/41NbdDuf0EL._SX331_BO1,204,203,200_.jpg'),
-('9782974329-23', 'Algebra - The Very Basics', 'Αγγλικά', 'If you''re looking for a gentle introduction to basic mathematics, look no further. This book picks you up at the very beginning and guides you through the foundations of algebra using lots of examples and no-nonsense explanations. Each chapter contains well-chosen exercises as well as all the solutions. No prior knowledge is required. ', '2014-01-06', 'book', 'http://ecx.images-amazon.com/images/I/51oESGdCPbL._SX312_BO1,204,203,200_.jpg', 'http://ecx.images-amazon.com/images/I/51oESGdCPbL._SX312_BO1,204,203,200_.jpg');
+('1568585047', 'And the Weak Suffer What They Must?', 'Αγγλικά', 'Europe has dragged the world into hideous morasses twice in the last one hundred years… it can do it again. Yanis Varoufakis, the former Finance Minister of Greece, shows here that the Eurozone is a house of cards destined to fall without a radical change in direction. And, if the European Union falls apart, he argues, the global economy will not be far behind.', '2014-01-04', 'magazine', 'http://ecx.images-amazon.com/images/I/51sDuJZNNnL._SX327_BO1,204,203,200_.jpg', 'http://ecx.images-amazon.com/images/I/51sDuJZNNnL._SX327_BO1,204,203,200_.jpg'),
+('178360610X', 'America, Europe and the Future of the Global Economy', 'Αγγλικά', 'In this provocative book, Yanis Varoufakis—the fiery former finance minister in Greek’s new Syriza-led government—explodes the myth that financialization, ineffective regulation of banks, and generalized greed and globalization were the root causes of the global economic crisis. Rather, he shows, they are symptoms of a much deeper malaise, one that can be traced all the way back to the Great Depression, then through the stagflation of the 1970s, when a “Global Minotaur” was born.', '2015-11-24', 'article', 'http://ecx.images-amazon.com/images/I/51zpkpTqZlL._SX324_BO1,204,203,200_.jpg', 'http://ecx.images-amazon.com/images/I/51zpkpTqZlL._SX324_BO1,204,203,200_.jpg'),
+('9782974329-23', 'Algebra - The Very Basics', 'Αγγλικά', 'If you''re looking for a gentle introduction to basic mathematics, look no further. This book picks you up at the very beginning and guides you through the foundations of algebra using lots of examples and no-nonsense explanations. Each chapter contains well-chosen exercises as well as all the solutions. No prior knowledge is required. ', '2014-01-06', 'book', 'http://ecx.images-amazon.com/images/I/51oESGdCPbL._SX312_BO1,204,203,200_.jpg', 'http://ecx.images-amazon.com/images/I/51oESGdCPbL._SX312_BO1,204,203,200_.jpg'),
+('B0006PV18O', 'What If?: Serious Scientific Answers to Absurd Hypothetical Questions', 'Αγγλικά', 'Millions of people visit xkcd.com each week to read Randall Munroe’s iconic webcomic. His stick-figure drawings about science, technology, language, and love have a large and passionate following.\r\n\r\nFans of xkcd ask Munroe a lot of strange questions. What if you tried to hit a baseball pitched at 90 percent the speed of light? How fast can you hit a speed bump while driving and live? If there was a robot apocalypse, how long would humanity last?', '2015-07-22', 'magazine', 'http://ecx.images-amazon.com/images/I/51hyJgRfUCL._SX383_BO1,204,203,200_.jpg', 'http://ecx.images-amazon.com/images/I/51hyJgRfUCL._SX383_BO1,204,203,200_.jpg'),
+('B00SV1T1VE', 'Greece and the Future of the Global Economy', 'Αγγλικά', 'Syriza’s victory in the Greek general election in January 2015 is a truly historic and profound event – the first government of the radical left in Europe since World War Two. In this short ebook, extracted from the updated edition of The Global Minotaur, Yanis Varoufakis, the new finance minister of Greece and a Syriza MP, outlines his economic and political thinking and how he believes Europe can move beyond cuts and austerity. ', '2016-01-12', 'article', 'http://ecx.images-amazon.com/images/I/51NMouy116L._SX324_BO1,204,203,200_.jpg', 'http://ecx.images-amazon.com/images/I/51NMouy116L._SX324_BO1,204,203,200_.jpg');
 
 -- --------------------------------------------------------
 
@@ -85,10 +94,16 @@ CREATE TABLE IF NOT EXISTS `Books_at_Libraries` (
 --
 
 INSERT INTO `Books_at_Libraries` (`book_isbn`, `library_id`, `quantity`) VALUES
+('0061710164', 3, 1),
 ('013937681X', 1, 5),
+('0544668251', 1, 3),
 ('123-77712832', 1, 4),
 ('1503379744A', 3, 2),
-('9782974329-23', 1, 3);
+('1568585047', 2, 1),
+('178360610X', 2, 1),
+('9782974329-23', 1, 3),
+('B0006PV18O', 1, 5),
+('B00SV1T1VE', 2, 1);
 
 -- --------------------------------------------------------
 
@@ -106,8 +121,16 @@ CREATE TABLE IF NOT EXISTS `Books_by_Category` (
 --
 
 INSERT INTO `Books_by_Category` (`book_isbn`, `category_id`) VALUES
+('0061710164', 4),
+('013937681X', 3),
+('0544668251', 2),
 ('123-77712832', 3),
-('9782974329-23', 1);
+('1503379744A', 4),
+('1568585047', 5),
+('178360610X', 5),
+('9782974329-23', 1),
+('B0006PV18O', 2),
+('B00SV1T1VE', 5);
 
 -- --------------------------------------------------------
 
@@ -125,11 +148,17 @@ CREATE TABLE IF NOT EXISTS `Book_Authors` (
 --
 
 INSERT INTO `Book_Authors` (`book_isbn`, `author_id`) VALUES
+('0061710164', 5),
 ('013937681X', 1),
+('0544668251', 6),
 ('123-77712832', 1),
 ('123-77712832', 2),
 ('1503379744A', 3),
-('9782974329-23', 4);
+('1568585047', 7),
+('178360610X', 7),
+('9782974329-23', 4),
+('B0006PV18O', 6),
+('B00SV1T1VE', 7);
 
 -- --------------------------------------------------------
 
@@ -163,7 +192,7 @@ INSERT INTO `Book_Loans` (`id`, `user_id`, `book_isbn`, `library_id`, `start_dat
 CREATE TABLE IF NOT EXISTS `Category` (
 `id` int(11) NOT NULL,
   `name` varchar(64) CHARACTER SET utf8 NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `Category`
@@ -172,7 +201,9 @@ CREATE TABLE IF NOT EXISTS `Category` (
 INSERT INTO `Category` (`id`, `name`) VALUES
 (1, 'Mathematics'),
 (2, 'Physics'),
-(3, 'Computer Science');
+(3, 'Computer Science'),
+(4, 'Theology'),
+(5, 'Economics');
 
 -- --------------------------------------------------------
 
@@ -215,7 +246,7 @@ CREATE TABLE IF NOT EXISTS `User` (
   `last_name` varchar(30) CHARACTER SET utf8 NOT NULL,
   `email` varchar(256) CHARACTER SET utf8 NOT NULL,
   `password` varchar(32) CHARACTER SET utf8 NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=52 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `User`
@@ -291,7 +322,7 @@ ALTER TABLE `User`
 -- AUTO_INCREMENT for table `Author`
 --
 ALTER TABLE `Author`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT for table `Book_Loans`
 --
@@ -301,7 +332,7 @@ MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 -- AUTO_INCREMENT for table `Category`
 --
 ALTER TABLE `Category`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `Library`
 --
@@ -311,7 +342,7 @@ MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 -- AUTO_INCREMENT for table `User`
 --
 ALTER TABLE `User`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=52;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
