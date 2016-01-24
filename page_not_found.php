b2c4a002bd75e758ce48aa0216977e65ea191e74
Konstantinos Triantafyllou <kostas.tri@hotmail.com>
2016-01-24 19:35:57 +0200

    Add two error pages.
    * Access denied error.
    * 404 page not found error.

---
+++ page_not_found.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <title>Η σελίδα δεν βρέθηκε</title>
+
+    <link rel="stylesheet" href="styles/main.css" />
+    <link rel="stylesheet" href="styles/error.css" />
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div id="error-template">
+                <h1>Oops!</h1>
+                <h2>404 Η σελίδα δεν βρέθηκε</h2>
+                <div id="error-details" class="big-text">
+                    Λυπούμαστε αλλά η διεύθυνση που ζητήσατε δεν αποτελεί σελίδα του ιστοτόπου.
+                </div>
+                <div id="error-actions">
+                    <a href="index.php" class="btn btn-primary btn-lg"><span class="glyphicon glyphicon-home"></span>&nbsp;Επιστροφή στην αρχική&nbsp;</a>
+                    <a href="" class="btn btn-default btn-lg"><span class="glyphicon glyphicon-envelope"></span>&nbsp;Επικοινωνείστε μαζί μας&nbsp;</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
