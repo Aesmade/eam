d0cf2df862232989634e94987c175b36b3d66ee4
Konstantinos Triantafyllou <kostas.tri@hotmail.com>
2016-01-26 11:35:12 +0200

    Add infowindow functionality to the libraries map.

--- include/php/queries/query_libraries.php
+++ include/php/queries/query_libraries.php
@@ -3,15 +3,15 @@
     {
         $libs = array();
 
-        $query = "SELECT id, name, address, latitude, longitude, telephone, email, " .
-            "opening_time, closing_time, img FROM `Library`";
+        $query = 'SELECT id, name, address, latitude, longitude, telephone, email,
+            description, opening_time, closing_time, img FROM `Library`';
         if (!$stmt = $db->prepare($query)) {
             echo "Prepare failed: (" . $db->errno . ") " . $db->error;
         }
         $stmt->execute();
         $templib = array();
         $stmt->bind_result($templib['id'], $templib['name'], $templib['address'], $templib['lat'], $templib['lon'],
-            $templib['phone'], $templib['email'], $templib['open'], $templib['close'], $templib['img']);
+            $templib['phone'], $templib['email'], $templib['description'], $templib['open'], $templib['close'], $templib['img']);
 
         while ($stmt->fetch()) {
             $lib = array();
